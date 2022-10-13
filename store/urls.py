f4fccc0a7b4cbbb75a1e69f68cd9871ddfee6919
kavyagodakumbura <110888851+kavyagodakumbura@users.noreply.github.com>
2022-10-14 00:36:54 +0530

    url

--- store/urls.py
+++ store/urls.py
@@ -13,7 +13,8 @@ urlpatterns = [
     path('', Index.as_view(), name='homepage'),
     path('store', store , name='store'),
 
-    
+    path('signup', Signup.as_view(), name='signup'),
+    path('login', Login.as_view(), name='login'),
     path('logout', logout , name='logout'),
     path('cart', auth_middleware(Cart.as_view()) , name='cart'),
     path('check-out', CheckOut.as_view() , name='checkout'),
