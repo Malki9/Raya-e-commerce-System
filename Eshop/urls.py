495f53145a80785af086ca93a00d21fae2dab8a5
Malki9 <113250751+Malki9@users.noreply.github.com>
2022-10-14 11:36:28 +0530

    Deleted the comment

--- Eshop/urls.py
+++ Eshop/urls.py
@@ -3,7 +3,7 @@ from django.urls import path  , include
 from django.conf.urls.static import static
 from . import settings
 
-#this is url path
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
